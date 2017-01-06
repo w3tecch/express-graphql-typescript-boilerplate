3aa40ea06b2a705d3ec0db6d2b0705a8e270c2af
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2017-01-07 00:25:37 +0100

    chore(): Add wallaby config

---
+++ wallaby.js
@@ -0,0 +1,41 @@
+module.exports = (wallaby) => {
+    process.env.NODE_ENV = 'test';
+    // View test statistics (for locally running wallaby): http://wallabyjs.com/app/#/tests
+    return {
+
+        env: {
+            // to specify system wide node as test runner, e.g. node 6 instead of node 4 from wallaby
+            // add "runner: '/usr/local/bin/node'" containing the correct path to your node
+            type: 'node'
+        },
+
+        files: [
+            { pattern: 'src/**/*.ts' },
+            { pattern: 'test/**/*.ts' },
+            { pattern: '!src/**/*.d.ts' },
+            { pattern: '!test/**/*.spec.ts' }
+        ],
+
+        tests: [
+            { pattern: 'test/**/*.spec.ts' }
+        ],
+
+        debug: true,
+
+        testFramework: 'jasmine',
+
+        compilers: {
+            '**/*.ts': wallaby.compilers.typeScript({
+                typescript: require('typescript'),
+                module: 'commonjs'
+            })
+        },
+
+        workers: {
+            recycle: true,
+            initial: 1,
+            regular: 1
+        }
+
+    };
+};
