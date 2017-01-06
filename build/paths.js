6b71f8feb390c44a562d1ec939e9e74a3f5883e3
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2017-01-07 00:01:13 +0100

    chore(): Add basic gulp setup

---
+++ build/paths.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * PATHS
+ * Defines the app-structure of this project
+ */
+module.exports = {
+    main: 'index.ts',
+    root: 'src',
+    test: 'test',
+    docs: 'docs',
+    dist: 'dist'
+};
