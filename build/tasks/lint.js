184a9952911f3b9ca9dac9163074df56a9b165ff
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2017-01-07 00:05:38 +0100

    chore(gulp): Add lint task

---
+++ build/tasks/lint.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const gulp = require('gulp');
+const path = require('path');
+const paths = require('../paths');
+const util = require('../util');
+const $ = require('gulp-load-plugins')({
+    lazy: true
+});
+
+gulp.task('lint', [
+    'lint:root',
+    'lint:test'
+]);
+
+gulp.task('lint:root', () => linter(paths.root));
+gulp.task('lint:test', () => linter(paths.test));
+
+function linter(filePath) {
+    return gulp
+        .src(['./typings/main.d.ts', path.join(filePath, '/**/*.ts')])
+        .pipe($.tslint({
+            emitError: false,
+            formatter: 'verbose'
+        }))
+        .pipe($.tslint.report())
+        .on('error', function() {
+            util.notify('TSLINT failed!');
+        });
+}
