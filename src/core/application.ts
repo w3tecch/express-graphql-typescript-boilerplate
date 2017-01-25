3989067a2578e107cda087aa01b61b4d1b0ee009
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2017-01-25 22:52:57 +0100

    refactor(core): Refactor core application file

--- src/core/application.ts
+++ src/core/application.ts
@@ -1,14 +1,6 @@
 //See: https://medium.com/the-graphqlhub/your-first-graphql-server-3c766ab4f0a2#.giyyd9tzx
 
 import * as express from 'express';
-import * as morgan from 'morgan';
-import * as cors from 'cors';
-import * as http from 'http';
-
-import * as Debug from 'debug';
-const debug = Debug('app:utils:application');
-
-import { logger } from './logger';
 
 const normalizePort = (val: string): number | string | boolean => {
     const parsedPort = parseInt(val, 10);
@@ -25,6 +17,5 @@ export const init = () => express();
 
 export const run = (app, port) => {
     port = normalizePort(port);
-    debug('Starts server on port ', port);
     return app.listen(port);
 };
