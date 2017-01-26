94194156b2087b72fcf5993fd5628360b2aea363
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2017-01-26 17:28:08 +0100

    feat(core): Add database file

---
+++ src/core/database.ts
@@ -0,0 +1,18 @@
+import * as Sequelize from 'sequelize';
+import * as cls from 'continuation-local-storage';
+
+import { Logger } from './logger';
+const log = Logger('app:database');
+
+import { configuration } from './environment';
+const environment = configuration();
+
+const namespace = cls.createNamespace('my-sequelize');
+// Use template string because else typescript thinks this is a constant (even with normal strings)
+Sequelize[`cls`] = namespace;
+
+log.debug('Connecting to database %s', environment.database);
+export const db = new Sequelize(environment.database, {
+    // disable logging; default: console.log or false
+    logging: log.debug
+});
