78a3351d3b39861e5fe79043b1c35e5b16f9b04c
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2017-02-20 07:35:57 +0100

    refactoring project

--- src/common/exceptions.ts
+++ src/common/exceptions.ts
@@ -1,5 +1,6 @@
 import * as Sequelize from 'sequelize';
 
+
 const name = (model: Sequelize.Model<any, any>) => model['name'];
 
 export const NotFound = (model: Sequelize.Model<any, any>) => (id?: number | string) => {
